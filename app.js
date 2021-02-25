5db9ee9466493f63ad2624e6f4fbfdb542710ccb
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-02-24 21:39:37 -0600

    Separación de yargs
    Se crea el archivo yargs aparte y se importa al archivo principal

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 const { crearTablaMultiplicar } = require( './helpers/multiplicar.js' );
-const argv = require('yargs')
-        .option( 'b', {
-            alias: 'base',
-            type: 'number',
-            demandOption: true
-        } )
-        .option( 'l', {
-            alias: 'listar',
-            type: 'boolean',
-            demandOption: true,
-            default: false
-        } )
-        .check( (argv, options) => {
-            if( isNaN( argv.b ) ){
-                throw 'La base tiene que ser un numero;'
-            }
-            return true;
-        } )
-        .argv;
+const argv  = require('./config/yargs.js');
 
 console.clear();
 
@@ -25,8 +7,6 @@ console.clear();
 console.log( process.argv );
 console.log( argv );
 
-
-console.log( 'base: yargs', argv.base );
 
 /*
 const [ , , arg3 = 'base=5 '] = process.argv; 
