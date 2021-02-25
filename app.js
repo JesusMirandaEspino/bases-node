155116356f11dfce8393c5ac09b9181c8329d630
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-02-24 20:49:03 -0600

    package json
    Uso de package json

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { crearTablaMultiplicar } = require( './helpers/multiplicar.js' );
 
 console.clear();
+
+
 /*
 const [ , , arg3 = 'base=5 '] = process.argv; 
 const [ , base = 5 ] = arg3.split('=');
